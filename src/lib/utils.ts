eb2df87667448995704094238742dfd380ce234f
Mael FOSSO <fosso.mael.elvis@gmail.com>
2024-09-17 06:31:47 +0100

    [DDX-210] integrated shadcn-ui components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...args: ClassValue[]) {
+  return twMerge(clsx(args));
+}
+
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
